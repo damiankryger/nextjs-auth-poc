631bb5b85a8d121f6ccadd8b562e8d46a15fc3a8
Damian Kryger <damiankryger@gmail.com>
2024-02-17 23:38:08 +0100

    (#18) (feat) Create main page with welcome message and logout button - changes main page adding message and logout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+'use client'
+
+import {Button} from "@nextui-org/react";
+import {signOut} from "next-auth/react";
+
 export default function Home() {
   return (
-    <div>Hello World!</div>
+    <div className={'h-screen w-screen flex flex-col items-center justify-center'}>
+      <h1 className={'text-2xl font-bold mb-4'}>Udało Ci się zalogować</h1>
+      <Button color={'primary'} onClick={() => signOut({callbackUrl: '/login'})}>A teraz się wyloguj</Button>
+    </div>
   );
 }
